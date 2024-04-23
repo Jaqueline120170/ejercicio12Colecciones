41397df3449c686366ce5e36692c3c98cc44cd12
Jaqueline <csi23-jromres@colegioaltair.net>
2024-04-23 12:16:14 +0200

    no message

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ejercicio2Colecciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> clientes = new Queue<string>();
+            clientes.Enqueue("Cliente 1");
+            clientes.Enqueue("Cliente 2");
+            clientes.Enqueue("Cliente 3");
+            clientes.Enqueue("Cliente 4");
+            clientes.Enqueue("Cliente 5");
+            clientes.Enqueue("Cliente 6");
+            clientes.Enqueue("Cliente 7");
+
+
+            foreach(string cliente in clientes)
+            {
+                Console.WriteLine(cliente);
+            }
+            Console.WriteLine("Nuevo primer elemento de la cola: " + clientes.Peek());
+
+            while (clientes.Count > 0)
+            {
+                string clienteActual = clientes.Dequeue();
+                Console.WriteLine("Atendiendo a: " + clienteActual);
+                
+            }
+        }
+    }
+}
